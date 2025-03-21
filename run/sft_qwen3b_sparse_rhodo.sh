f786b8b6cd5214c2abd65d628c263924d3014832
rsadhukh <rsadhukh@andrew.cmu.edu>
2025-03-21 03:23:57 -0400

    added 3b scripts

---
+++ run/sft_qwen3b_sparse_rhodo.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+OUTPUT_DIR=/sensei-fs/users/xuhuang/rsadhukh/LLaMA-Factory
+export ALLOW_EXTRA_ARGS=1
+topk=$1
+local=$2
+ctx_len=$3
+
+llamafactory-cli train \
+    --model_name_or_path Qwen/Qwen2.5-3B-Instruct \
+    --trust_remote_code true \
+    --flash_attn fa2 \
+    --stage sft \
+    --do_train true \
+    --sparse_training \
+    --sparse_attn_topk ${topk} \
+    --sparse_attn_local ${local} \
+    --sparse_attn_sink 4 \
+    --deepspeed examples/deepspeed/ds_z3_config.json \
+    --dataset open_r1_math \
+    --template qwen \
+    --cutoff_len $ctx_len \
+    --overwrite_cache true \
+    --preprocessing_num_workers 16 \
+    --output_dir ${OUTPUT_DIR}/saves/qwen2.5-3b/full/sft_sparse_ctx${ctx_len} \
+    --logging_steps 10 \
+    --save_steps 1000 \
+    --plot_loss true \
+    --overwrite_output_dir true \
+    --per_device_train_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --enable_liger_kernel true \
+    --learning_rate 5.0e-5 \
+    --weight_decay 0.0001 \
+    --num_train_epochs 5.0 \
+    --lr_scheduler_type linear \
+    --warmup_ratio 0.1 \
+    --bf16 true \
+    --ddp_timeout 180000000 \
+    --save_total_limit 1 \
+    --push_to_hub --export_hub_model_id Rano23/OpenR1-qwen-3b-sft-sparse_ctx${ctx_len}
